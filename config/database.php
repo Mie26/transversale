3244a43403a542fd8e156225cd0a5a59531373ca
Diamondra03433 <diamondrakarneol@gmail.com>
2025-07-13 08:56:04 +0200

    push

--- config/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Connexion MySQL simple (PHP5)
-mysql_connect("localhost", "root", "") or die("Erreur de connexion à la base");
-mysql_select_db("clubs_universitaires") or die("Base inconnue");
-mysql_query("SET NAMES 'utf8'");
-?>
--- controllers/pages/deconnexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("../config/database.php");
-require_once("../config/session.php");
-
-deconnecter();
-header("Location: index.php");
-exit();
-?>
